a1d7a090b4484aca4dc197a35b4bb5a865beed4c
jonata azevedo <manojhonson181@gmail.com>
2022-09-05 22:53:12 -0300

    first commit

---
+++ src/main/java/estudos/spring/java/EstudosSpringBootJavaApplication.java
@@ -0,0 +1,13 @@
+package estudos.spring.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstudosSpringBootJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstudosSpringBootJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/estudos/spring/java/EstudosSpringBootJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package estudos.spring.java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstudosSpringBootJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
